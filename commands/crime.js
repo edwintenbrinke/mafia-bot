b8c8d9f9cf755a97ae0a208ac791aef8f7e500b5
EdwinExtendas <edwin.ten.brinke@extendas.com>
2019-02-16 22:23:34 +0100

    wtf lol

---
+++ commands/crime.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, msg) => {
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: "crime",
+    description: "do a crime",
+    usage: "crime"
+};
